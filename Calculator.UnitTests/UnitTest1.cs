deab2faef3b2ecd97798915be46c9312cab5a955
Ilya Fedosov <ilya.fedosov@antidasoftware.com>
2020-07-13 19:45:19 +0500

    Add new test

--- Calculator.UnitTests/UnitTest1.cs
+++ Calculator.UnitTests/UnitTest1.cs
@@ -30,9 +30,26 @@ namespace Calculator.UnitTests
             Assert.AreEqual(expectedResult, actualResult);
         }
 
+        [Test]
+        [TestCaseSource("CompoundExpressionsSuccess")]
+        public void CompoundExpressionCalculateSuccess(string expression, double expectedResult)
+        {
+            var actualResult = calculatorService.Calculate(expression);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        static object[] CompoundExpressionsSuccess =
+        {
+            new object[] { "3+9-6", 6 },
+            new object[] { "7+5*3", 22 },
+            new object[] { "7*(5+9)+9", 1107 },
+            new object[] { "(5+6)/(7+9)*8", 0.0859375 }
+        };
+
         static object[] UnlimitedOperandsExpressionsSuccess =
         {
             new object[] { "7+9+6", 22 },
+            new object[] { "1+2+3+4+5+6+7+8+9", 45 },
             new object[] { "38-20-9", 9 },
             new object[] { "8*3*7", 168 },
             new object[] { "102/2/5", 10.2 }
