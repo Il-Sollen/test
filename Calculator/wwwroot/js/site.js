adf590e95232dc150f54652914ccf75e20989198
Il-Sollen <sollen53@gmail.com>
2020-07-14 00:41:15 +0500

    Add FrontEnd part

--- Calculator/wwwroot/js/site.js
+++ Calculator/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function calculate() {
+    $.ajax({
+        url: "test.html",
+        context: document.body,
+        success: function () {
+            $(this).addClass("done");
+        }
+    });    
+}
