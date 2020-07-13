7bf447d524e9235fdee4ad26f0983c92d042cf9d
Ilya Fedosov <ilya.fedosov@antidasoftware.com>
2020-07-13 19:32:20 +0500

    Add new test

--- Calculator.UnitTests/UnitTest1.cs
+++ Calculator.UnitTests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Calculator.Services;
 using NUnit.Framework;
+using System;
 
 namespace Calculator.UnitTests
 {
@@ -14,14 +15,30 @@ namespace Calculator.UnitTests
         }
 
         [Test]
-        [TestCaseSource("PrimitiveExpressions")]
-        public void PrimitiveExpressionSuccessCalculate(string expression, double expectedResult)
+        [TestCaseSource("PrimitiveExpressionsSuccess")]
+        public void PrimitiveExpressionCalculateSuccess(string expression, double expectedResult)
         {
             var actualResult = calculatorService.Calculate(expression);
             Assert.AreEqual(expectedResult, actualResult);
         }
 
-        static object[] PrimitiveExpressions =
+        [Test]
+        [TestCaseSource("UnlimitedOperandsExpressionsSuccess")]
+        public void UnlimitedOperandsExpressionsCalculateSuccess(string expression, double expectedResult)
+        {
+            var actualResult = calculatorService.Calculate(expression);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        static object[] UnlimitedOperandsExpressionsSuccess =
+        {
+            new object[] { "7+9+6", 22 },
+            new object[] { "38-20-9", 9 },
+            new object[] { "8*3*7", 168 },
+            new object[] { "102/2/5", 10.2 }
+        };
+
+        static object[] PrimitiveExpressionsSuccess =
         {
             new object[] { "2+3", 5 },
             new object[] { "6-3", 3 },
