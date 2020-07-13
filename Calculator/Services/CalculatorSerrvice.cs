8394a98f820cdd25b33c7519999b721b322b9986
Ilya Fedosov <ilya.fedosov@antidasoftware.com>
2020-07-13 20:54:37 +0500

    Create parse expression to Reverse Polish notation method

--- Calculator/Services/CalculatorSerrvice.cs
+++ Calculator/Services/CalculatorSerrvice.cs
@@ -59,5 +59,95 @@ namespace Calculator.Services
 
             return result;
         }
+
+
+
+        private static Queue<TokenBase> ParseToRPN(string input)
+        {
+            var outputQueue = new Queue<TokenBase>();
+            var operationsStack = new Stack<TokenBase>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsDigit(input[i]))
+                {
+                    var number = string.Empty;
+                    while (char.IsDigit(input[i]))
+                    {
+                        number += input[i];
+                        i++;
+
+                        if (i == input.Length)
+                            break;
+                    }
+
+                    outputQueue.Enqueue(new Number(number));
+                    i--;
+                }
+                else
+                {
+                    var operation = ParseOperation(input[i]);
+
+                    if (operation is OpenParenthesis)
+                        operationsStack.Push(operation);
+
+                    else if (operation is CloseParenthesis)
+                    {
+                        var stackOperstion = operationsStack.Pop();
+                        while (!(stackOperstion is OpenParenthesis))
+                        {
+                            outputQueue.Enqueue(stackOperstion);
+                            stackOperstion = operationsStack.Pop();
+                        }
+                    }
+                    else
+                    {
+                        if (operationsStack.Count > 0)
+                        {
+                            if (operation.Weight <= operationsStack.Peek().Weight)
+                                outputQueue.Enqueue(operationsStack.Pop());
+                        }
+                        operationsStack.Push(operation);
+                    }
+                }
+
+            }
+            while (operationsStack.Any())
+                outputQueue.Enqueue(operationsStack.Pop());
+
+            return outputQueue;
+        }
+
+        private static double CalculateRPNExpression(Queue<TokenBase> stack)
+        {
+            var tempStack = new Queue<double>();
+            while (stack.Count != 0)
+            {
+                var token = stack.Dequeue();
+
+                if (token is Number t)
+                {
+                    tempStack.Enqueue(t.Value);
+                }
+                else
+                {
+                    var a = tempStack.Dequeue();
+                    var b = tempStack.Dequeue();
+                    tempStack.Enqueue(token.Calc(a, b));
+                }
+            }
+
+            return tempStack.Peek();
+        }
+
+        private static TokenBase ParseOperation(char operation) => operation switch
+        {
+            '+' => new Addition(),
+            '-' => new Subtraction(),
+            '*' => new Multiplication(),
+            '/' => new Division(),
+            '(' => new OpenParenthesis(),
+            ')' => new CloseParenthesis(),
+            _ => throw new Exception($"Unknown operation: {operation}"),
+        };
     }
 }
